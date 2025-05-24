f585e2656833579080ab44f2494a190b27754418
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-05-24 14:58:57 +0600

    card

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Product = () => {
   return (
-    <div>Product</div>
+    <div>
+        <div className="">
+            <div className="p-10 bg-gray-200 ">
+                <h2 className='text-2xl font-bold pb-10'>Recent Products </h2>
+                <div className="bg-white w-60 py-5 px-4 ">
+                    <div className="mb-5">
+                        <img src="./product1.jpg" alt="" />
+                    </div>
+                    <div className="">
+                        <h2 className='text-gray-500 pb-3'>Rubbermaid</h2>
+                        <p className='text-blue-300 pb-8'>7gal. Blcak Reactangular Wastebasket</p>
+                        <p>$5.85</p>
+                    </div>
+                    <div className="">
+                        <div className="">
+                            
+                        </div>
+                        <p>@4 Reviews</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
